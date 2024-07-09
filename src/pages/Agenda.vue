121afb0f17c921ed6ae4dae6672406e72ff983c7
Gabriel Gustavo Queiroz <gabrielgustavosantosqueiroz@gmail.com>
2024-07-09 20:27:43 -0300

    Revert "Agenda Funcionando"
    This reverts commit 1382134928dd344ebf218de18d2e42dae833b3e6.

--- src/pages/Agenda.vue
+++ src/pages/Agenda.vue
@@ -1,86 +1,101 @@
 <template>
   <q-page padding>
-    <div class="agendamento-titulo">Agendados</div>
-    <hr class="linha-separadora" />
+  <div class="agendamento-titulo">Agendados</div>
+  <hr class="linha-separadora" />
 
-    <div class="agendamento-lista">
-      <q-card flat bordered v-for="agendamento in agendamentos" :key="agendamento.id" class="agendamento-card q-mb-md">
-        <q-card-section>
-          <div class="text-h6">{{ agendamento.cliente }}</div>
-          <div>{{ agendamento.data }} {{ agendamento.hora }}</div>
-          <div v-if="agendamento.remarcado" class="remarcado-tag">Remarcado</div>
-        </q-card-section>
+  <div class="agendamento-lista">
+    <q-card flat bordered v-for="agendamento in agendamentos" :key="agendamento.id" class="agendamento-card q-mb-md">
+      <q-card-section>
+        <div class="text-h6">{{ agendamento.cliente }}</div>
+        <div>{{ agendamento.data }}</div>
+        <div v-if="agendamento.remarcado" class="remarcado-tag">Remarcado</div>
+      </q-card-section>
 
-        <q-card-actions>
-          <q-btn icon="done" @click="concluirAgendamento(agendamento)" />
-          <q-btn icon="event" @click="abrirDialogRemarcar(agendamento)" />
-          <q-btn icon="delete" @click="excluirAgendamento(agendamento.id)" />
-        </q-card-actions>
-      </q-card>
-    </div>
+      <q-card-actions>
+        <q-btn icon="done" @click="concluirAgendamento(agendamento)" />
+        <q-btn icon="event" @click="abrirDialogRemarcar(agendamento)" />
+        <q-btn icon="delete" @click="excluirAgendamento(agendamento.id)" />
+      </q-card-actions>
+    </q-card>
+  </div>
 
-    <div class="agendamento-titulo">Concluídos</div>
-    <hr class="linha-separadora" />
+  <div class="agendamento-titulo">Concluídos</div>
+  <hr class="linha-separadora" />
 
-    <div class="agendamento-lista">
-      <q-card flat bordered v-for="agendamento in agendamentosConcluidos" :key="agendamento.id" class="agendamento-card q-mb-md">
-        <q-card-section>
-          <div class="text-h6">{{ agendamento.cliente }}</div>
-          <div>{{ agendamento.data }} {{ agendamento.hora }}</div>
-        </q-card-section>
+  <div class="agendamento-lista">
+    <q-card flat bordered v-for="agendamento in agendamentosConcluidos" :key="agendamento.id" class="agendamento-card q-mb-md">
+      <q-card-section>
+        <div class="text-h6">{{ agendamento.cliente }}</div>
+        <div>{{ agendamento.data }}</div>
+      </q-card-section>
 
-        <q-card-actions>
-          <!-- Adicionar botões específicos para agendamentos concluídos, se necessário -->
-        </q-card-actions>
-      </q-card>
-    </div>
+      <q-card-actions>
+        <!-- Adicionar botões específicos para agendamentos concluídos, se necessário -->
+      </q-card-actions>
+    </q-card>
+  </div>
 
-    <q-btn color="primary" label="Novo Agendamento" @click="mostrarDialogNovoAgendamento = true" />
+  <q-btn color="primary" label="Novo Agendamento" @click="mostrarDialogNovoAgendamento = true" />
 
-    <!-- Diálogo para Remarcação de Agendamento -->
-    <q-dialog v-model="mostrarDialogRemarcar" persistent>
-      <q-card>
-        <q-card-section>
-          <q-date v-model="novaDataSelecionada" label="Nova Data" dense />
-          <q-time v-model="novaHoraSelecionada" label="Nova Hora" format="24hr" dense />
-        </q-card-section>
+  <!-- Diálogo para Remarcação de Agendamento -->
+  <q-dialog v-model="mostrarDialogRemarcar" persistent>
+    <q-card>
+      <q-card-section>
+        <q-date v-model="novaDataSelecionada" label="Nova Data" dense />
+      </q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn label="Cancelar" color="secondary" @click="cancelarRemarcacao" />
-          <q-btn label="Remarcar" color="primary" @click="remarcarAgendamento" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+      <q-card-actions align="right">
+        <q-btn label="Cancelar" color="secondary" @click="cancelarRemarcacao" />
+        <q-btn label="Remarcar" color="primary" @click="remarcarAgendamento" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 
-    <!-- Diálogo para Novo Agendamento -->
-    <q-dialog v-model="mostrarDialogNovoAgendamento" persistent>
-      <q-card>
-        <q-card-section>
-          <q-select
-            v-model="clienteSelecionado"
-            :options="clientes"
-            label="Cliente"
-            dense
-            emit-value
-            map-options
-          />
-          <q-date v-model="dataSelecionada" label="Data" dense />
-          <q-time v-model="horaSelecionada" label="Hora" format="24hr" dense />
-        </q-card-section>
+  <!-- Diálogo para Novo Agendamento -->
+  <q-dialog v-model="mostrarDialogNovoAgendamento" persistent>
+    <q-card>
+      <q-card-section>
+        <q-select
+          v-model="clienteSelecionado"
+          :options="clientes"
+          label="Cliente"
+          dense
+          emit-value
+          map-options
+        />
+        <q-date v-model="dataSelecionada" label="Data" dense />
+      </q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn label="Cancelar" color="secondary" @click="cancelarNovoAgendamento" />
-          <q-btn label="Adicionar" color="primary" @click="adicionarAgendamento" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-  </q-page>
+      <q-card-actions align="right">
+        <q-btn label="Cancelar" color="secondary" @click="cancelarNovoAgendamento" />
+        <q-btn label="Adicionar" color="primary" @click="adicionarAgendamento" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import axios from 'axios';
-import {
+import { QBtn, QDialog, QCard, QCardSection, QCardActions, QSelect, QDate } from 'quasar';
+
+interface Agendamento {
+id: number;
+cliente: string;
+data: string;
+remarcado?: boolean;
+concluido?: boolean; // Adiciona a propriedade concluido
+}
+interface Cliente {
+  nome: string;
+  // adicione outros campos conforme necessário
+}
+
+
+export default defineComponent({
+name: 'AgendaPage',
+components: {
   QBtn,
   QDialog,
   QCard,
@@ -88,197 +103,133 @@ import {
   QCardActions,
   QSelect,
   QDate,
-  QTime,
-} from 'quasar';
+},
+setup() {
+  const agendamentos = ref<Agendamento[]>([]);
+  const agendamentosConcluidos = ref<Agendamento[]>([]);
+  const clientes = ref<string[]>([]);
+  const mostrarDialogNovoAgendamento = ref(false);
+  const mostrarDialogRemarcar = ref(false);
+  const clienteSelecionado = ref<string | null>(null);
+  const dataSelecionada = ref<string | null>(null);
+  const novaDataSelecionada = ref<string | null>(null);
+  let agendamentoParaRemarcar: Agendamento | null = null;
 
-interface Agendamento {
-  id: number;
-  cliente: string;
-  data: string;
-  hora: string;
-  remarcado?: boolean;
-  concluido?: boolean;
-}
+  const apiBaseUrl = 'http://localhost:3000';
 
-interface Cliente {
-  nome: string;
-  // adicione outros campos conforme necessário
-}
+  const fetchAgendamentos = async () => {
+    try {
+      const response = await axios.get(`${apiBaseUrl}/agendamentos`);
+      agendamentos.value = response.data.filter((agendamento: Agendamento) => !agendamento.concluido);
+      agendamentosConcluidos.value = response.data.filter((agendamento: Agendamento) => agendamento.concluido);
+    } catch (error) {
+      console.error('Erro ao buscar agendamentos:', error);
+    }
+  };
 
-export default defineComponent({
-  name: 'AgendaPage',
-  components: {
-    QBtn,
-    QDialog,
-    QCard,
-    QCardSection,
-    QCardActions,
-    QSelect,
-    QDate,
-    QTime,
-  },
-  setup() {
-    const agendamentos = ref<Agendamento[]>([]);
-    const agendamentosConcluidos = ref<Agendamento[]>([]);
-    const clientes = ref<string[]>([]);
-    const mostrarDialogNovoAgendamento = ref(false);
-    const mostrarDialogRemarcar = ref(false);
-    const clienteSelecionado = ref<string | null>(null);
-    const dataSelecionada = ref<string | null>(null);
-    const horaSelecionada = ref<string | null>(null);
-    const novaDataSelecionada = ref<string | null>(null);
-    const novaHoraSelecionada = ref<string | null>(null);
-    let agendamentoParaRemarcar: Agendamento | null = null;
+  const fetchClientes = async () => {
+    try {
+      const response = await axios.get(`${apiBaseUrl}/clientes`);
+      clientes.value = response.data.map((cliente: Cliente) => cliente.nome);// Ajuste conforme a estrutura dos dados do cliente
+    } catch (error) {
+      console.error('Erro ao buscar clientes:', error);
+    }
+  };
 
-    const apiBaseUrl = 'http://localhost:3000';
+  onMounted(() => {
+    fetchAgendamentos();
+    fetchClientes();
+  });
 
-    const fetchAgendamentos = async () => {
+  const adicionarAgendamento = async () => {
+    if (clienteSelecionado.value && dataSelecionada.value) {
       try {
-        const response = await axios.get(`${apiBaseUrl}/agendamentos`);
-        agendamentos.value = response.data.filter((agendamento: Agendamento) => !agendamento.concluido);
-        agendamentosConcluidos.value = response.data.filter((agendamento: Agendamento) => agendamento.concluido);
+        const response = await axios.post(`${apiBaseUrl}/agendamentos`, {
+          cliente: clienteSelecionado.value,
+          data: dataSelecionada.value,
+        });
+        agendamentos.value.push(response.data);
+        cancelarNovoAgendamento();
       } catch (error) {
-        console.error('Erro ao buscar agendamentos:', error);
+        console.error('Erro ao adicionar agendamento:', error);
       }
-    };
+    }
+  };
 
-    const fetchClientes = async () => {
+  const cancelarNovoAgendamento = () => {
+    mostrarDialogNovoAgendamento.value = false;
+    clienteSelecionado.value = null;
+    dataSelecionada.value = null;
+  };
+
+  const abrirDialogRemarcar = (agendamento: Agendamento) => {
+    agendamentoParaRemarcar = agendamento;
+    novaDataSelecionada.value = agendamento.data;
+    mostrarDialogRemarcar.value = true;
+  };
+
+  const cancelarRemarcacao = () => {
+    mostrarDialogRemarcar.value = false;
+    novaDataSelecionada.value = null;
+    agendamentoParaRemarcar = null;
+  };
+
+  const remarcarAgendamento = async () => {
+    if (novaDataSelecionada.value && agendamentoParaRemarcar) {
       try {
-        const response = await axios.get(`${apiBaseUrl}/clientes`);
-        clientes.value = response.data.map((cliente: Cliente) => cliente.nome);
+        await axios.put(`${apiBaseUrl}/agendamentos/${agendamentoParaRemarcar.id}`, {
+          data: novaDataSelecionada.value,
+          remarcado: true,
+        });
+        agendamentoParaRemarcar.data = novaDataSelecionada.value;
+        agendamentoParaRemarcar.remarcado = true;
+        cancelarRemarcacao();
       } catch (error) {
-        console.error('Erro ao buscar clientes:', error);
+        console.error('Erro ao remarcar agendamento:', error);
       }
-    };
+    }
+  };
 
-    onMounted(() => {
-      fetchAgendamentos();
-      fetchClientes();
-    });
+  const concluirAgendamento = async (agendamento: Agendamento) => {
+    try {
+      await axios.put(`${apiBaseUrl}/agendamentos/${agendamento.id}`, { concluido: true });
+      agendamentos.value = agendamentos.value.filter(a => a.id !== agendamento.id);
+      agendamentosConcluidos.value.push(agendamento);
+    } catch (error) {
+      console.error('Erro ao concluir agendamento:', error);
+    }
+  };
 
-    const adicionarAgendamento = async () => {
-      if (clienteSelecionado.value && dataSelecionada.value && horaSelecionada.value) {
-        try {
-          const response = await axios.post(`${apiBaseUrl}/agendamentos`, {
-            cliente: clienteSelecionado.value,
-            data: dataSelecionada.value,
-            hora: horaSelecionada.value,
-          });
-          agendamentos.value.push(response.data);
-          cancelarNovoAgendamento();
-        } catch (error) {
-          console.error('Erro ao adicionar agendamento:', error);
-        }
-      }
-    };
+  const excluirAgendamento = async (agendamentoId: number) => {
+    try {
+      await axios.delete(`${apiBaseUrl}/agendamentos/${agendamentoId}`);
+      agendamentos.value = agendamentos.value.filter(a => a.id !== agendamentoId);
+    } catch (error) {
+      console.error('Erro ao excluir agendamento:', error);
+    }
+  };
 
-    const cancelarNovoAgendamento = () => {
-      mostrarDialogNovoAgendamento.value = false;
-      clienteSelecionado.value = null;
-      dataSelecionada.value = null;
-      horaSelecionada.value = null;
-    };
-
-    const abrirDialogRemarcar = (agendamento: Agendamento) => {
-      agendamentoParaRemarcar = agendamento;
-      novaDataSelecionada.value = agendamento.data;
-      novaHoraSelecionada.value = agendamento.hora;
-      mostrarDialogRemarcar.value = true;
-    };
-
-    const cancelarRemarcacao = () => {
-      mostrarDialogRemarcar.value = false;
-      novaDataSelecionada.value = null;
-      novaHoraSelecionada.value = null;
-      agendamentoParaRemarcar = null;
-    };
-
-    const remarcarAgendamento = async () => {
-      if (novaDataSelecionada.value && novaHoraSelecionada.value && agendamentoParaRemarcar) {
-        try {
-          await axios.put(`${apiBaseUrl}/agendamentos/${agendamentoParaRemarcar.id}`, {
-            data: novaDataSelecionada.value,
-            hora: novaHoraSelecionada.value,
-            remarcado: true,
-          });
-          agendamentoParaRemarcar.data = novaDataSelecionada.value;
-          agendamentoParaRemarcar.hora = novaHoraSelecionada.value;
-          agendamentoParaRemarcar.remarcado = true;
-          cancelarRemarcacao();
-        } catch (error) {
-          console.error('Erro ao remarcar agendamento:', error);
-        }
-      }
-    };
-
-    const concluirAgendamento = async (agendamento: Agendamento) => {
-      try {
-        await axios.put(`${apiBaseUrl}/agendamentos/${agendamento.id}`, { concluido: true });
-        agendamentos.value = agendamentos.value.filter(a => a.id !== agendamento.id);
-        agendamentosConcluidos.value.push(agendamento);
-      } catch (error) {
-        console.error('Erro ao concluir agendamento:', error);
-      }
-    };
-
-    const excluirAgendamento = async (agendamentoId: number) => {
-      try {
-        await axios.delete(`${apiBaseUrl}/agendamentos/${agendamentoId}`);
-        agendamentos.value = agendamentos.value.filter(a => a.id !== agendamentoId);
-      } catch (error) {
-        console.error('Erro ao excluir agendamento:', error);
-      }
-    };
-
-    return {
-      agendamentos,
-      agendamentosConcluidos,
-      clientes,
-      mostrarDialogNovoAgendamento,
-      mostrarDialogRemarcar,
-      clienteSelecionado,
-      dataSelecionada,
-      horaSelecionada,
-      novaDataSelecionada,
-      novaHoraSelecionada,
-      adicionarAgendamento,
-      cancelarNovoAgendamento,
-      abrirDialogRemarcar,
-      cancelarRemarcacao,
-      remarcarAgendamento,
-      concluirAgendamento,
-      excluirAgendamento,
-    };
-  },
+  return {
+    agendamentos,
+    agendamentosConcluidos,
+    clientes,
+    mostrarDialogNovoAgendamento,
+    mostrarDialogRemarcar,
+    clienteSelecionado,
+    dataSelecionada,
+    novaDataSelecionada,
+    adicionarAgendamento,
+    cancelarNovoAgendamento,
+    abrirDialogRemarcar,
+    cancelarRemarcacao,
+    remarcarAgendamento,
+    concluirAgendamento,
+    excluirAgendamento,
+  };
+},
 });
 </script>
 
-
-
 <style scoped>
-.agendamento-titulo {
-  font-size: 1.5rem;
-  margin-bottom: 0.5rem;
-}
-
-.linha-separadora {
-  margin-top: 0.5rem;
-  margin-bottom: 0.5rem;
-}
-
-.agendamento-lista {
-  margin-bottom: 1rem;
-}
-
-.agendamento-card {
-  width: 100%;
-}
-
-.remarcado-tag {
-  background-color: #ffcc80;
-  color: #333;
-  padding: 0.25rem 0.5rem;
-  border-radius: 4px;
-  font-weight: bold;
-}
+/* Seu estilo permanece o mesmo */
 </style>
